f5e0f192f9c8d41953d2bb865d12510cb01fb0bb
yomnahegazy <119704857+yomnahegazy@users.noreply.github.com>
2022-12-02 23:03:01 +0200

    Add files via upload

---
+++ ins/ins/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ins
+{
+    internal class InsertionSort
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 11, 13, 5, 6 };
+            InsertionSort ob = new InsertionSort();
+            sort(arr);
+            printArray(arr);
+            Console.ReadKey();
+        }
+        
+
+
+            public static void sort(int[] arr)
+            {
+                int n = arr.Length;
+                for (int i = 1; i < n; ++i)
+                {
+                    int key = arr[i];
+                    int j = i - 1;
+
+
+                    while (j >= 0 && arr[j] > key)
+                    {
+                        arr[j + 1] = arr[j];
+                        j = j - 1;
+                    }
+                    arr[j + 1] = key;
+                }
+            }
+
+
+         static   void printArray(int[] arr)
+            {
+                int n = arr.Length;
+                for (int i = 0; i < n; ++i)
+                    Console.Write(arr[i] + " ");
+
+                Console.Write("\n");
+            }
+    }    }
+
